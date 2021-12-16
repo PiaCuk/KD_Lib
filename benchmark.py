153299d484e4c6b33793749709dbb0f33419f190
Pia Cuk <pia.cuk@gmx.de>
2021-12-16 12:39:32 +0100

    Benchmark with final hyperparams

--- benchmark.py
+++ benchmark.py
@@ -95,38 +95,22 @@ if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
     # Use new universal main
-    main(
-        "dml",
-        5,
-        100,
-        1024,
-        "/data1/9cuk/kd_lib/hyperparams/adamw-003-10-False",
-        loss_fn=CustomKLDivLoss(apply_softmax=True), # only used for DML
-        lr=0.003,
-        distil_weight=0.5,
-        temperature=10.0,
-        num_students=3,
-        use_pretrained=False,
-        use_scheduler=True,
-        use_weighted_dl=False,
-        schedule_distil_weight=False,
-        seed=42
-    )
-
-    main(
-        "dml",
-        5,
-        100,
-        1024,
-        "/data1/9cuk/kd_lib/hyperparams/adamw-003-10-True",
-        loss_fn=CustomKLDivLoss(apply_softmax=True), # only used for DML
-        lr=0.003,
-        distil_weight=0.5,
-        temperature=10.0,
-        num_students=3,
-        use_pretrained=False,
-        use_scheduler=True,
-        use_weighted_dl=False,
-        schedule_distil_weight=True,
-        seed=42
-    )
+    for algo in ["dml", "dml_e", "tfkd", "vanilla"]:
+        params = {
+            "algo": algo,
+            "runs": 5,
+            "epochs": 100,
+            "batch_size": 1024,
+            "save_path": "/data1/9cuk/kd_lib/final_hyperparams",
+            "loss_fn": CustomKLDivLoss(apply_softmax=algo != "dml_e"),
+            "lr": 0.005,
+            "distil_weight": 0.5,
+            "temperature": 10.0 if algo != "tfkd" else 1.0,
+            "num_students": 3,
+            "use_pretrained": True,
+            "use_scheduler": True,
+            "use_weighted_dl": False,
+            "schedule_distil_weight": False,
+            "seed": 42,
+        }
+        main(**params)
